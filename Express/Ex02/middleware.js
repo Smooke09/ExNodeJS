7f343a951021fdbf5440e959fdee7caef492fcc9
Smooke09 <pedrolucas1014zz@gmail.com>
2022-07-04 12:15:42 -0300

    Exercise Method USE

---
+++ Express/Ex02/middleware.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const server = express();
+
+//Cadeia de middleware
+server.get("/", function (req, res, next) {
+  console.log("Inicio...");
+  next();
+  console.log("Fim...");
+});
+
+server.get("/", function (req, res, next) {
+  console.log("Resposta...");
+  res.send("<h1>Ola express</h1>");
+});
+
+server.listen(3000, () => [console.log("Executando...")]);
